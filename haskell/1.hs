23b8e97440b30ac061d52d9637140d4bbf71d589
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2016-02-18 16:32:11 +0100

    The software would loop unwanted when you'd execute faca/facb with a negative number. Now this is solved.

--- haskell/1.hs
+++ haskell/1.hs
@@ -1,8 +1,9 @@
 faca::Int->Int
 faca 0 = 1
-faca x = x * faca(x-1)
+faca x = if x > 0 then x * faca(x-1) else x * faca(x * (-1) - 1)
 
 facb::Int->Int
 facb x
      | x == 0 = 1
+     | x < 0 = x * facb(x * (-1) - 1)
      | otherwise = x * facb(x-1)
