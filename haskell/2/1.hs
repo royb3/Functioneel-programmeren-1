bac68f1f9ea7c1ea8923e49e871d8badb34486bb
Roy Buitenhuis <roy.buitenhuis94@gmail.com>
2016-04-13 16:42:41 +0200

    Added 2 and started with 3

--- haskell/2/1.hs
+++ haskell/2/1.hs
@@ -1,3 +1,5 @@
+import Data.Ord
+import Data.Char
 --1A
 euclid::Integer->Integer->Integer
 euclid x y
@@ -11,8 +13,8 @@ egcd a b =
    let (g, s, t) = egcd (b `mod` a) a
     in (g, t - (b `div` a) * s, s)
 
-p = 7
-q = 11
+p = 101
+q = 113
 m = p * q
 m' = (p-1) * (q-1)
 
@@ -24,6 +26,28 @@ eigenEgcd :: Integer -> Integer -> (Integer, Integer, Integer)
 eigenEgcd 0 b = (b, 0, 1)
 eigenEgcd a b =
   let
-    u(g, s, t) = egcd(b `mod` a) a
+    (g, s, t) = egcd(b `mod` a) a
     middelsste = t - (b `div` a) * s
-  in (g, if middelsste < 0 then (middelsste + b) else middelsste, s)
+    in (g, if middelsste < 0 then (middelsste + m) else middelsste, s)
+mogelijkeGetallenVoorE = [ e | e <- [0..m'-1], (gcd e m') == 1]
+mogelijkeGetallenVoorD e = [ d | d <- [0..40000] ,(e * d) `mod` m' == 1 ]
+
+privateKey = 241
+publicKey = 7761
+
+
+
+rsaencrypt::(Integer,Integer)->Integer->Integer
+rsaencrypt (key,modulus) c = (c ^ key) `mod` modulus
+
+rsaencryptChar::(Integer, Integer)->Char->Integer
+rsaencryptChar (key,modulus) char = let c = toInteger $ord char in (c ^ key) `mod` modulus
+
+
+rsadecrypt::(Integer,Integer)->Integer->Integer
+rsadecrypt (key,modulus) c = (c ^ key) `mod` modulus
+
+rsadecryptChar::(Integer, Integer)->Integer->Char
+rsadecryptChar (key,modulus) c = chr $fromInteger $(c ^ key) `mod` modulus
+
+--5: Alice versleuteld de berichten voor Bob doormiddel van Bob's public key. Zo kan alleen bob deze weer decrypten. :)
--- haskell/3/1.hs
+++ haskell/3/1.hs
@@ -1,7 +1,11 @@
 f :: Double->Double
-f x = x^3
+f x = 2 * x
 
 diff :: (Double->Double)->Double->Double->Double
 diff f p x = ((f(x + p) - f(x)) / p )
 
-integral :: (Double->Double)->Double->Double->Double
+antidiff :: (Double->Double)->Double->Double->Double
+antidiff f p x = ((f(x - p) + f(x)) * p )
+
+integral :: (Double->Double)->Double->Double->Double->Double
+integral f a b p = (antidiff f p b ) -( antidiff f p a)
